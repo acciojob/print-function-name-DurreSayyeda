8d498a0bc2b33b6a4a0b2a073354b66210422bb7
Sayyeda Afshan <syed1995.sa@gmail.com>
2023-12-07 21:06:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function functionName() {
+    alert(arguments.callee.name);
+}
+
+// Test case
+functionName(); // This will display an alert with the text "functionName"
